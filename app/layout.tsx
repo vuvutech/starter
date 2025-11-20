532be6ce7664224b9eba0c51c8e24677b9334f47
Pius Opoku-Fofie <pius1919@gmail.com>
2025-11-20 17:57:58 +0000

    updates ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <main className="mx-auto max-w-5xl py-10">{children}</main>
+        {children}
       </body>
     </html>
   );
--- types/institute.ts
+++
@@ -1,5 +0,0 @@
-import { type Institute, type Edition } from "@prisma/client";
-
-export type InstituteWithEditions = Institute & {
-  editions: Edition[];
-};
